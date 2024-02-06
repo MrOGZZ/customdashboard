12c8e280713527b85559b106e1f780f93b6d567d
Gokay Evrenos <gomemarketings@gmail.com>
2024-02-06 15:55:44 +0300

    commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
 
 app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
